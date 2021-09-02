2693b21abfff810ae3e655d2ac0a8fe22f559af0
AkashDeep000 <akashdeepdas970@gmail.com>
2021-09-02 09:59:44 +0530

    fdd

--- components/PdfWidget.js
+++ components/PdfWidget.js
@@ -44,7 +44,7 @@ const handlePdfUpload = async (e) => {
 //Make the request to the POST /single-file URL
   
 // const response = await axios.post( 'http://15.207.86.194:5000/upload',
-const response = await axios.post( 'http://15.207.86.194:5000/upload',
+const response = await axios.post( 'http://pdf-api.ddns.net/upload',
 
   formData,
   {
