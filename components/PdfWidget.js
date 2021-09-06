9fb9310ab5e289f3ef80abf2d8ad5a78856cd98e
AkashDeep000 <akashdeepdas970@gmail.com>
2021-09-06 11:58:42 +0530

    gfdd

--- components/PdfWidget.js
+++ components/PdfWidget.js
@@ -216,7 +216,7 @@ display: "inlineBlock",
     background: "rgb(255,167,190)",
 backgroundImage: "url(\"data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ff5c86' fill-opacity='0.3'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
 textShadow: "0 0 2rem rgb(255,129,213)",
-  }} download="cc-02.pdf">Download Compressed PDF<span style={{color:"rgb(78,152,255)"}}>{pdfRes?.size ? ` (${pdfRes.size}KB)` : null}</span></a>
+  }} download>Download Compressed PDF<span style={{color:"rgb(78,152,255)"}}>{pdfRes?.size ? ` (${pdfRes.size}KB)` : null}</span></a>
   
   {uploadState == "Progressing PDF..." ? (<div className="spinner" role="spinner"><div className="spinner-icon"></div></div>) : null}
   
